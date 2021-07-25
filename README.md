b6fa713c0e4f4b4f38d0485d6feb0546646031f6
CiGit-Franck <franck.thery@gmail.com>
2021-07-25 22:26:51 +0200

    at tested

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # **esp32-cam**
-https://fr.aliexpress.com/item/1005001726282043.html
-Ref
-https://docs.espressif.com/projects/esp-idf/en/latest/esp32/index.html#
-https://learn.upesy.com/fr/index.html
-https://randomnerdtutorials.com/projects-esp32-cam/
---
-http://192.168.0.100/capture
+>-Product : https://fr.aliexpress.com/item/1005001726282043.html
+>### Goals
+>-**Detect motion (PIR) -> capture -> MQTT**
+>>-with IP static
+>>-with Manage interrupt on esp32 (PIR)
+>>-with capture from MQTT
+>### Resultat
+>>-http://192.168.0.100/capture
+>### Reference :
+>>-https://docs.espressif.com/projects/esp-idf/en/latest/esp32/index.html#
+>>-https://learn.upesy.com/fr/index.html
+>>-https://randomnerdtutorials.com/projects-esp32-cam/
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,34 @@
 /*
  * Detection motion -> capture -> node-red
  *
- * PIR <-+-> ESP GPIO2
+ * PIR <-+-> ESP GPIO12
  *       |
  *       +--> MQTT --> Node-RED
- *             |          |
- *             +--> http://[espIP]/capture
+ *       |     |          |
+ *       +-----+--> http://[espIP]/capture
  * 
  */
 
-#include <esp32cam.h>         // Simply manage OV2640 : https://github.com/yoursunny/esp32cam
 #include <WiFi.h>
 #include <WebServer.h>
-#include <PubSubClient.h>     // MQTT
+#include <esp32cam.h>         // Simply manage OV2640 : https://github.com/yoursunny/esp32cam
+#include <PubSubClient.h>     // MQTT : https://github.com/knolleary/pubsubclient
 #include <soc/soc.h>          // Manage interrupt
 #include <soc/rtc_cntl_reg.h> // Manage interrupt
-#include "Credential.h"       // your private credential WiFi
+#include "Credential.h"       // Your private credential WiFi
 
 #define PIN_FLASH 4 // flah board linked on GPIO4
 #define ESP_NAME "ESP32-Cam"
-#define TOPIC_CAMERASHOT "cam/demo/shot"
+#define TOPIC_CAMERASHOT "cam/demo"
+#define PIN_MOTION GPIO_NUM_12
 
 WebServer server(80);
 IPAddress espIP(192, 168, 0, 100); // Define static IP for dns, gatewaty & subnet put in Credential.h
 
 WiFiClient espClient;
 PubSubClient clientMQTT(mqttServer, mqttPort, espClient);
-char message_buff[100];
 
-void handleJpeg()
+void capture()
 {
   digitalWrite(PIN_FLASH, HIGH);
   delay(100);
@@ -51,21 +51,9 @@ void handleJpeg()
 
 void callback(char *topic, byte *payload, unsigned int length)
 {
-  Serial.print("Message arrived: [");
-  Serial.print(topic);
-  Serial.println("]"); // Prints out any topic that has arrived and is a topic that we subscribed to.
-
-  int i;
-  for (i = 0; i < length; i++)
-  {
-    message_buff[i] = payload[i];
-  }
-  message_buff[i] = '\0'; // We copy payload to message_buff because we can't make a string out of a byte based payload.
-
-  String msgString = String(message_buff); // Converting our payload to a string so we can compare it.
   if (strcmp(topic, TOPIC_CAMERASHOT) == 0)
   {
-    handleJpeg();
+    capture();
   }
 }
 
@@ -80,7 +68,7 @@ void connectWifi()
       ;
 
     // WiFi connected -> start server
-    server.on("/capture", handleJpeg);
+    server.on("/capture", capture);
     server.begin();
   }
   delay(100);
@@ -92,7 +80,7 @@ void connectWifi()
       clientMQTT.subscribe(TOPIC_CAMERASHOT);
     }
     else
-    { // failed with state
+    { 
       delay(5e3);
     }
   }
@@ -110,27 +98,9 @@ void initCam()
   delay(100);
 }
 
-static void IRAM_ATTR motionDetect(void *arg)
+void detectsMovement()
 {
-  clientMQTT.publish(TOPIC_CAMERASHOT, "", false);
-}
-
-  
-void addExternalInterrupt()
-{
-  // PIR Motion Sensor
-  esp_err_t err = gpio_install_isr_service(0);
-  err = gpio_isr_handler_add(GPIO_NUM_2, &motionDetect, (void *)2);
-  if (err != ESP_OK)
-  {
-    Serial.printf("handler add failed with error 0x%x \r\n", err);
-  }
-  err = gpio_set_intr_type(GPIO_NUM_2, GPIO_INTR_POSEDGE); // arduino rising mode
-  if (err != ESP_OK)
-  {
-    Serial.printf("set intr type failed with error 0x%x \r\n", err);
-  }
-  delay(100);
+  capture();
 }
 
 void setup()
@@ -139,13 +109,16 @@ void setup()
   Serial.begin(115200);
   delay(100);
   pinMode(PIN_FLASH, OUTPUT);
-  addExternalInterrupt();
+  digitalWrite(PIN_FLASH, LOW);
+  pinMode(PIN_MOTION, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(PIN_MOTION), detectsMovement, RISING);
+  delay(100);
   // init cam
   initCam();
-  // init wifi
+  // init WiFi
   WiFi.mode(WIFI_STA);
-  WiFi.config(espIP, dns, gateway, subnet);
   WiFi.setHostname(ESP_NAME);
+  WiFi.config(espIP, dns, gateway, subnet);
   delay(100);
   // init MQTT
   clientMQTT.setServer(mqttServer, mqttPort);
